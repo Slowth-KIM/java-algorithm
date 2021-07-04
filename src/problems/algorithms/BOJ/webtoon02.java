a3985c0f5e0376a8182cbb50c0e53b60e55b47ca
Slowth-KIM <rt.slowth@gmail.com>
2021-07-04 16:26:01 +0900

    네이버 웹툰 코테 참가

---
+++ src/problems/algorithms/BOJ/webtoon02.java
@@ -0,0 +1,33 @@
+package problems.algorithms.BOJ;
+
+import java.util.*;
+
+public class webtoon02 {
+
+    public int solution(String s, String t) {
+        int result = 0;
+        int length = t.length();
+        int sLength = s.length();
+
+
+        //s 안에 t가 없으면 루프를 빠져나오도록 한다.
+        while (s.contains(t)) {
+            int start = s.indexOf(t);
+            int end = start + length - 1;
+            sLength = s.length();
+
+            //start와 end를 기준으로 앞 뒤로 잘라서 다시 s를 정의함
+            s = s.substring(0, start) + s.substring(end + 1, sLength);
+            result += 1;
+        }
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        webtoon02 webtoon02 = new webtoon02();
+        System.out.println(webtoon02.solution("aabcbcd"	, "abc"));
+        System.out.println(webtoon02.solution("aaaaabbbbb", "ab"));
+    }
+}
---
+++ src/problems/main.java
@@ -0,0 +1,10 @@
+package problems;
+
+import problems.algorithms.BOJ.webtoon02;
+
+public class main {
+    public static void main(String[] args) {
+        webtoon02 webtoon02 = new webtoon02();
+
+    }
+}
